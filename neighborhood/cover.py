c093c1fbc908d8d5ea9b2212c8ae992493dc7d34
Ubuntu <ehretb24@mail.wlu.edu>
2022-05-02 15:55:54 +0000

    Adding Assignment 4.

--- neighborhood/cover.py
+++ neighborhood/cover.py
@@ -2,10 +2,10 @@
 
 import os
 import math
+import sys
 
 
-
-def neighborhood(file1, newFile):
+def neighborhood(file1, newFile, distance):
     lyst = []
     count = 0
     lystCover = []
@@ -16,7 +16,7 @@ def neighborhood(file1, newFile):
 
     for i in lyst:
         for j in lyst:
-            if math.sqrt(((int(j[0]) - int(i[0]))**2 + (int(j[1]) - int(i[1]))**2)) <= 25:
+            if math.sqrt(((int(j[0]) - int(i[0]))**2 + (int(j[1]) - int(i[1]))**2)) <= int(distance):
                 if j!= i:
                     lyst.pop(lyst.index(j))   
         lystCover.append(i)
@@ -30,19 +30,26 @@ def neighborhood(file1, newFile):
 
 
 def main():
-    numbers1 = open(os.path.dirname(__file__) + "/../datasets/numbers1.txt", "r")
-    numbers2 = open(os.path.dirname(__file__) + "/../datasets/numbers2.txt", "r")
-    newFile3 = open("cover_1.txt", "w")
-    newFile2 = open("cover_2.txt", "w")
-    neighborhood(numbers1, newFile3)
-    neighborhood(numbers2, newFile2)
+    #numbers1 = open(os.path.dirname(__file__) + "/../datasets/numbers1.txt", "r")
+    #numbers2 = open(os.path.dirname(__file__) + "/../datasets/numbers2.txt", "r")
+    #newFile3 = open("cover_1.txt", "w")
+    #newFile2 = open("cover_2.txt", "w")
+    #neighborhood(numbers1, newFile3)
+    #neighborhood(numbers2, newFile2)
 
-    print("done")
-    numbers1.close()
-    numbers2.close()
-    newFile2.close()
-    newFile3.close()
+    #print("done")
+    #numbers1.close()
+    #numbers2.close()
+    #newFile2.close()
+    #newFile3.close()
 
+    #code to use command line args
+    inputName = sys.argv[1]
+    outputName = sys.argv[2]
+    distance = sys.argv[3]
+    inputFile = open(str(inputName),"r")
+    outputFile = open(str(outputName), "w")
+    neighborhood(inputFile, outputFile, distance)
 
 
 if __name__ == "__main__":
--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -1,10 +1,16 @@
 
 import os
+import sys
 
-def wordcount(fileName, outputFile):
+def wordcount(fileName, outputFile, stopFile):
     dictionary = {}
     punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     count = 0
+    stopWords = []
+
+    for line in stopFile:
+        for word in line.split(','):
+            stopWords.append(word)
 
     for line in fileName:
         for word in line.split():
@@ -12,7 +18,7 @@ def wordcount(fileName, outputFile):
             for char in word:
                 if char in punctuation:
                     word = word.replace(char, "")
-            if word != "":
+            if word != "" and word not in stopWords:
                 if not word in dictionary.keys():
                     dictionary[word] = 1
                 else:
@@ -35,29 +41,39 @@ def wordcount(fileName, outputFile):
 
 
 def main():
-    file1 = open(os.path.dirname(__file__) + "/../datasets/cards.txt", 'r')
-    file1Out = open("freq_cards.txt", 'w')
-    file2 = open(os.path.dirname(__file__) + "/../datasets/article_text.txt", 'r')
-    file2Out = open("freq_article_text.txt", 'w')
-    file3 = open(os.path.dirname(__file__) + "/../datasets/headlines.txt", 'r')
-    file3Out = open("freq_headlines.txt", 'w')
-    file4 = open(os.path.dirname(__file__) + "/../datasets/descriptions.txt", 'r')
-    file4Out = open("freq_descriptions.txt", 'w')
-    wordcount(file1, file1Out)
-    wordcount(file2, file2Out)
-    wordcount(file3, file3Out)
-    wordcount(file4, file4Out)
-    print("done")
-    file1.close()
-    file1Out.close()
-    file2.close()
-    file2Out.close()
-    file3.close()
-    file3Out.close()
-    file4.close()
-    file4Out.close()
+    #file1 = open(os.path.dirname(__file__) + "/../datasets/cards.txt", 'r')
+    #file1Out = open("freq_cards.txt", 'w')
+    #file2 = open(os.path.dirname(__file__) + "/../datasets/article_text.txt", 'r')
+    #file2Out = open("freq_article_text.txt", 'w')
+    #file3 = open(os.path.dirname(__file__) + "/../datasets/headlines.txt", 'r')
+    #file3Out = open("freq_headlines.txt", 'w')
+    #file4 = open(os.path.dirname(__file__) + "/../datasets/descriptions.txt", 'r')
+    #file4Out = open("freq_descriptions.txt", 'w')
+    #wordcount(file1, file1Out)
+    #wordcount(file2, file2Out)
+    #wordcount(file3, file3Out)
+    #wordcount(file4, file4Out)
+    #print("done")
+    #file1.close()
+    #file1Out.close()
+    #file2.close()
+    #file2Out.close()
+    #file3.close()
+    #file3Out.close()
+    #file4.close()
+    #file4Out.close()
     
-
+    # adding command line functionality
+    inputName = sys.argv[1]
+    outputName = sys.argv[2]
+    stopName = sys.argv[3]
+    inputFile = open(inputName, "r")
+    outputFile = open(outputName, "w")
+    stopFile = open(stopName, "r")
+    wordcount(inputFile, outputFile, stopFile)
+    inputFile.close()
+    outputFile.close()
+    stopFile.close()
 
 
 if __name__ == "__main__":
